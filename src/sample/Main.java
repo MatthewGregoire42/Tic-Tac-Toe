b6c595a9ffff874829c78669e62c076a13bc5cba
Matthew <mattyg@live.unc.edu>
2020-06-13 18:51:04 -0400

    Started project

---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // Sets window title
+        primaryStage.setTitle("Hello World");
+        // Sets contents of the window as a 300x275 box
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
